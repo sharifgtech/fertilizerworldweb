520ed2267b49d0a36f30ace28e96b96e0c2685bc
sharifgtech <80701213+sharifgtech@users.noreply.github.com>
2025-08-31 15:20:32 +0530

    third commit
    third commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "39112458"
+    serviceWorkerVersion: "4094094302"
   }
 });
