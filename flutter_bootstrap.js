51c15259e4bcb27a7e7926b7a04c33fed991abca
sharifgtech <80701213+sharifgtech@users.noreply.github.com>
2025-11-01 22:20:03 +0530

    sds
    second changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "283825342"
+    serviceWorkerVersion: "2369711462"
   }
 });
